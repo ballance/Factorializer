105cb1d3a6ef63e85d58b7edc10f1f74bbe51952
Chris Ballance <ballance@gmail.com>
2016-03-19 09:51:59 -0400

    Initial chicken Completed v1

---
+++ Factorializer/Factorializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ballance.IT
+{
+    public class Factorializer
+    {
+        private static void Main()
+        {
+            var firstInput = Console.ReadLine();
+
+            // Handling as Int64 due to limitation of Int32.Max of 2,147,483,647
+            UInt64 firstInteger = 1;
+            if (!UInt64.TryParse(firstInput, out firstInteger))
+            {
+                Console.WriteLine("ERROR");
+                return;
+            }
+
+            UInt64 factorialized = 1;
+
+            for (UInt64 i = firstInteger; i > 0; i--)
+            {
+                factorialized *= i;
+            }
+
+
+            Console.WriteLine(factorialized);
+
+            var secondInput = Console.ReadLine();
+
+            UInt64 secondInteger = 0;
+            if (!UInt64.TryParse(secondInput, out secondInteger))
+            {
+                Console.WriteLine("ERROR");
+                return;
+            }
+
+            Console.WriteLine(factorialized == secondInteger ? "TRUE" : "FALSE");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Factorializer
+
+Takes in integer input and returns the factorial of the integer.  This works up to a point, then exceeds the Int64 capacity and I haven't spent much time on the high end edge case.
